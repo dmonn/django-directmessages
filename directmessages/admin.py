329ef9c51b8c46783d3285472af5421f3fc93ff4
Dominic Monn <dm@impac.ch>
2016-07-24 21:13:59 +0200

    changed admin list display

--- directmessages/admin.py
+++ directmessages/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 
 class MessageAdmin(admin.ModelAdmin):
     model = Message
-    list_display = ('id', 'sender', 'recipient', 'content', )
+    list_display = ('id', 'sender', 'content', )
 
 admin.site.register(Message, MessageAdmin)
