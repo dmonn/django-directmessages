117ec1172b8e51092d476dba45953951807494a6
Dominic Monn <dm@impac.ch>
2016-07-26 08:23:26 +0200

    django import exception handling

--- directmessages/__init__.py
+++ directmessages/__init__.py
@@ -1,3 +1,8 @@
-from django import VERSION as DJANGO_VERSION
-if DJANGO_VERSION >= (1, 7):
-    default_app_config = 'directmessages.apps.DirectmessagesConfig'
+__version__ = '0.9.2'
+
+try:
+    from django import VERSION as DJANGO_VERSION
+    if DJANGO_VERSION >= (1, 7):
+        default_app_config = 'directmessages.apps.DirectmessagesConfig'
+except ImportError:
+    pass
